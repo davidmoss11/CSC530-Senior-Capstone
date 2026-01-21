5402a0a661eddb642ea0c2ef2020b05edaf30277
Polemistis Akontio <samlpritchett@gmail.com>
2026-01-21 15:30:59 -0500

    Created a Test Button

--- app/src/main/java/com/example/simplifyyourpantry/MainActivity.java
+++ app/src/main/java/com/example/simplifyyourpantry/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.*;
+import android.widget.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +22,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button newButton = findViewById(R.id.button1);
+        newButton.setOnClickListener(v -> {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = inflater.inflate(R.layout.popup_window, null);
+            int width = ViewGroup.LayoutParams.WRAP_CONTENT;
+            int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            boolean focusable = true;
+
+            final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+            popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+            popupView.setOnTouchListener(new View.OnTouchListener(){
+               @Override
+               public boolean onTouch(View v, MotionEvent event) {
+                   v.performClick();
+                   popupWindow.dismiss();
+                   return true;
+               };
+            });
         });
     }
 }
