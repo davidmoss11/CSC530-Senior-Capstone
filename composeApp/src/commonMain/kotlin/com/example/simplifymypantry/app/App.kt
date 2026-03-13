9f2853ec221862a4e96085eb6513b73b3934548b
davidmoss11 <dmoss6@murraystate.edu>
2026-03-12 22:28:45 -0500

    Fixed syntax and merge conflicts

--- composeApp/src/commonMain/kotlin/com/example/simplifymypantry/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/simplifymypantry/app/App.kt
@@ -39,7 +39,7 @@ fun App() {
         NavHost(
             navController = navController,
             startDestination = Route.AppGraph
-        ){
+        ) {
              navigation<Route.AppGraph>(
                  startDestination = Route.LoginPage
              ){
