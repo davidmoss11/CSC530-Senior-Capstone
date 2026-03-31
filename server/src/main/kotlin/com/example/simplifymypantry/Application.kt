8f099258f8870d4cd581018644740b28991329e1
Polemistis Akontio <samlpritchett@gmail.com>
2026-03-31 12:55:07 -0400

    Pushing backend

--- server/src/main/kotlin/com/example/simplifymypantry/Application.kt
+++ server/src/main/kotlin/com/example/simplifymypantry/Application.kt
@@ -7,16 +7,20 @@ import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import com.example.simplifymypantry.config.*
+import com.example.simplifymypantry.service.*
+import com.example.simplifymypantry.database.*
 import com.example.simplifymypantry.util.SimpleJWT
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.plugins.contentnegotiation.*
+import org.koin.ktor.plugin.koin
+import org.koin.ktor.ext.inject
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
 
-
-    install(Authentication)
     install(StatusPages) {
         statusPages()
     }
@@ -24,7 +28,28 @@ fun Application.module() {
 
     }
 
-    val simpleJWT = SimpleJWT(secret = environment.config.property("jwt.secret").getString())
+    val serviceKoinModule = module {
+        single<IAuthService> { AuthService(get()) }
+    }
+
+    val databaseKoinModule = module {
+        single<IDatabaseFactory> { DatabaseFactory(environment) }
+    }
+
+    install(Koin) {
+        modules(
+            serviceKoinModule,
+            databaseKoinModule
+        )
+    }
+
+    val simpleJWT = SimpleJWT(secret = "test-secret")
+
+    //val simpleJWT = SimpleJWT(secret = environment.config.property("jwt.secret").getString())
+
+    install(Authentication) {
+        jwtConfig(simpleJWT)
+    }
 
     routing {
         api(simpleJWT)
